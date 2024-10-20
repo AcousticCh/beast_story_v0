11115ee027810cdb77da23ed145a7b91a7274490
Robert Hassenmayer <hassenr@protonmail.com>
2024-10-20 16:46:09 -0400

    Create beast_story_v0.c

---
+++ beast_story_v0.c
@@ -0,0 +1,10 @@
+/* beast story v.0 */
+
+#include <stdio.h>
+
+int main() 
+{
+    char name[] = "Robert";
+    printf("Welcome %c you are entering a new world where fantasy becomes reality and uncertainty is all around.\n", name[]);
+    printf("The world is divided by race and you be pushed into this distress although you are not trapped\n");
+}
